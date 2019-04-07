a10db7905eac1976b16779c8795e3ac53cefc5ac
Michael Hicks <kustomdesigner@Michaels-MBP.lan1>
2019-04-07 16:06:52 -0400

    Created a simple node api

---
+++ index.js
@@ -0,0 +1,31 @@
+/*
+*    Simple Node API
+*/
+
+// Dependencies
+const http = require('http');
+const port = 3000;
+
+// Create Server
+http.createServer(function (req, res) {
+    // Default route
+    if('/' == req.url) {
+      res.writeHead(200, { 'Content-Type' : 'text/html'});
+      res.end('Welcome to my app.');
+
+    // Hello route 
+    } else if ('/hello' == req.url) {
+        res.writeHead(200, { 'Content-Type' : 'text/html'});
+        let message = {'message' : 'Hello, welcome to my app.'};
+        res.write(JSON.stringify(message));
+        res.end();
+        
+    // Not found route
+    } else {
+      res.writeHead(200, { 'Content-Type' : 'text/html'});
+      let message = {'error' : 'page not found'};
+      res.write(JSON.stringify(message));
+      res.end();
+    }
+  console.log('App is started on port: ' + port);
+}).listen(port);
